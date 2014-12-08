7c70dd53e073ae2e75a7afb206ede4ece0c83d15
wilsaj <wilson.andrew.j+github@gmail.com>
2014-12-08 17:06:15 -0600

    post form data to contact app on submit

--- static/js/contact-forms.js
+++ static/js/contact-forms.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('FormApp', ['ngAnimate'])
-  .controller('FormController', ['$scope', function($scope) {
+  .controller('FormController', ['$scope', '$http', function($scope, $http) {
+    var contact_app_url = 'http://localhost:8001/';
+
     $scope.master = {};
     $scope.errors = {};
+    $scope.status = 'not submitted';
 
     $scope.submit = function(form) {
       $scope.master = angular.copy(form);
+
       _($scope.form)
         .filter(function(value, key) {return key[0] !== '$'})
         .each(function(item) {
@@ -14,7 +18,15 @@ angular.module('FormApp', ['ngAnimate'])
         });
 
       if (!$scope.form.$invalid) {
-        alert('submit');
+        $scope.status = 'submitting';
+
+        $http.post(contact_app_url, $scope.master)
+          .error(function(data, status, headers, config) {
+            $scope.status = 'error';
+          })
+          .success(function(data, status, headers, config) {
+            $scope.status = 'success';
+          });
       }
     };
 

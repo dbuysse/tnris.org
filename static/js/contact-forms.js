af31a9ab2fefab7264fc0ccbce5144a2059ddd49
wilsaj <wilson.andrew.j+github@gmail.com>
2014-12-11 11:50:00 -0600

    adjust reCAPTCHA box styling and add invalid/error message for it

--- static/js/contact-forms.js
+++ static/js/contact-forms.js
@@ -13,17 +13,20 @@ angular.module('FormApp', ['ngAnimate', 'grecaptcha'])
     $scope.master = {};
     $scope.errors = {};
     $scope.status = 'not submitted';
+    $scope.recaptcha = '';
 
     $scope.submit = function(form) {
       $scope.master = angular.copy(form);
 
       _($scope.form)
-        .filter(function(value, key) {return key[0] !== '$'})
+        .filter(function(value, key) {return key[0] !== '$';})
         .each(function(item) {
           $scope.errors[item.$name] = item.$invalid;
         });
 
-      if ($scope.form.$invalid) {
+      $scope.errors['recaptcha'] = !$scope.recaptcha;
+
+      if (_.any($scope.errors)) {
         $scope.status = 'invalid';
       } else {
         $scope.status = 'submitting';
@@ -38,13 +41,20 @@ angular.module('FormApp', ['ngAnimate', 'grecaptcha'])
       }
     };
 
-    $scope.updateItem = function (item){
+    $scope.updateItem = function (item) {
       $scope.errors[item.$name] = item.$invalid;
     };
 
     $scope.$watch('form.$invalid', function (value) {
       if (!value && $scope.status === 'invalid') {
         $scope.status = 'not submitted';
+      }
+    });
+
+    $scope.$watch('recaptcha', function (value) {
+      if (value && $scope.status === 'invalid') {
+        $scope.status = 'not submitted';
+        $scope.errors['recaptcha'] = !$scope.recaptcha;
       }
     });
   }]);

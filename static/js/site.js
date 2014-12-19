9178cf53e0ee0466b024555acc41a823c1c31697
James Seppi <james.seppi@gmail.com>
2014-12-19 15:25:29 -0600

    change indentation; wrap all in ready function

--- static/js/site.js
+++ static/js/site.js
@@ -1,47 +1,50 @@
 (function($) {
   'use strict';
-// Image Slider
-	$("#imageCompare1, #imageCompare2, #imageCompare3, #imageCompare4, #imageCompare5, #imageCompare6, #imageCompare7").twentytwenty({default_offset_pct: .5});
+
+  $(function() {
+  
+    // Image Slider
+    $("#imageCompare1, #imageCompare2, #imageCompare3, #imageCompare4, #imageCompare5, #imageCompare6, #imageCompare7").twentytwenty({default_offset_pct: .5});
 
 
-// Nav scroll spy
-	$('body').scrollspy({ target: '.wms-nav-container', offset: 130 });
+    // Nav scroll spy
+    $('body').scrollspy({ target: '.wms-nav-container', offset: 130 });
 
-// Tool Tip
-	$('.copy-tip').tooltip({'placement': 'top'});
-	$('.wms-tip').tooltip({'placement': 'bottom'});
+    // Tool Tip
+    $('.copy-tip').tooltip({'placement': 'top'});
+    $('.wms-tip').tooltip({'placement': 'bottom'});
 
-//zclip + ZeroClipboard url copy buttons
-$('.copy-url-container').each(function (index, node) {
-  var $node = node;
-  var $btn = $('.copy-url-btn', $node);
-  var origInner = $btn.html();
-  var val = $('.copy-url-input', $node).val();
-  $btn.zclip({
-    path: 'bower_components/jquery-zclip/ZeroClipboard.swf',
-    copy: val,
-    afterCopy: function () {
-      $btn.html('Copied!');
-      window.setTimeout(function () {
-        $btn.html(origInner);
-      }, 4000);
-    }
+    //zclip + ZeroClipboard url copy buttons
+    $('.copy-url-container').each(function (index, node) {
+      var $node = node;
+      var $btn = $('.copy-url-btn', $node);
+      var origInner = $btn.html();
+      var val = $('.copy-url-input', $node).val();
+      $btn.zclip({
+        path: 'bower_components/jquery-zclip/ZeroClipboard.swf',
+        copy: val,
+        afterCopy: function () {
+          $btn.html('Copied!');
+          window.setTimeout(function () {
+            $btn.html(origInner);
+          }, 4000);
+        }
+      });
+    });
+
+
+    $('.nav-cat').affix({
+      offset: {
+        top: function () {
+          return (this.top = $('header').outerHeight(true));
+        }
+       }
+    });
+
+    $("a[href^='\#']").each(function(){
+      this.href=location.href.split("#")[0]+'#'+this.href.substr(this.href.indexOf('#')+1);
+    });
+
   });
-
-});
-
-$().ready(function() {
-  $('.nav-cat').affix({
-    offset: {
-      top: function () {
-        return (this.top = $('header').outerHeight(true));
-      }
-     }
-  });
-
-  $("a[href^='\#']").each(function(){
-    this.href=location.href.split("#")[0]+'#'+this.href.substr(this.href.indexOf('#')+1);
-  });
-});
 
 })(jQuery);

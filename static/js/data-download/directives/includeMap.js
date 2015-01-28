dd7c600f195e5030948bb346193de55d9bcb57db
wilsaj <wilson.andrew.j+github@gmail.com>
2015-01-28 17:07:39 -0600

    set zoom to texasish

--- static/js/data-download/directives/includeMap.js
+++ static/js/data-download/directives/includeMap.js
@@ -5,7 +5,7 @@ var includeMap = function ($compile, $http) {
     link: {
       post: function ($scope, $element) {
         // create a map in the "map" div, set the view to a given place and zoom
-        var map = L.map($element[0]).setView([51.505, -0.09], 13);
+        var map = L.map($element[0]).setView([30, -97], 12);
 
         // add mapquest open aerial tile layer
         var baseLayer = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png',{

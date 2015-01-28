eea1d3b55e607ebe2a6204f8429de96d17564433
wilsaj <wilson.andrew.j+github@gmail.com>
2015-01-28 17:08:07 -0600

    fixup: base layer

--- static/js/data-download/directives/includeMap.js
+++ static/js/data-download/directives/includeMap.js
@@ -12,7 +12,7 @@ var includeMap = function ($compile, $http) {
           subdomains: '1234',
           attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
         });
-
+        baseLayer.addTo(map);
       }
     }
   };

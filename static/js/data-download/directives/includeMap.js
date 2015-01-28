20928f25c670694b471aa3e42e8b872d7cb73fba
wilsaj <wilson.andrew.j+github@gmail.com>
2015-01-28 17:20:22 -0600

    add cartodb layer from viz id

--- static/js/data-download/directives/includeMap.js
+++ static/js/data-download/directives/includeMap.js
@@ -1,4 +1,4 @@
-var includeMap = function ($compile, $http) {
+var includeMap = function ($compile, $http, CARTODB_API_URL, CARTODB_COUNTY_VIZ_ID, CARTODB_QUAD_VIZ_ID) {
   return {
     restrict: 'EA',
     template: '<div id="map"></div>',
@@ -13,6 +13,22 @@ var includeMap = function ($compile, $http) {
           attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
         });
         baseLayer.addTo(map);
+
+        var cartodb_layer_url = CARTODB_API_URL  + 'viz/' + CARTODB_QUAD_VIZ_ID + '/viz.json';
+
+        cartodb.createLayer(map, cartodb_layer_url)
+          .addTo(map)
+          .on('done', function(layer) {
+            layer.setInteraction(true);
+            layer.on('featureOver', function(e, pos, latlng, data) {
+              cartodb.log.log(e, pos, latlng, data);
+            });
+            layer.on('error', function(err) {
+              cartodb.log.log('error: ' + err);
+            });
+          }).on('error', function(a,b,c) {
+            cartodb.log.log("some error occurred");
+          });
       }
     }
   };

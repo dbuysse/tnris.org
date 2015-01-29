9ccfa23fa1e02e05d02d8d9a07637eb718055981
wilsaj <wilson.andrew.j+github@gmail.com>
2015-01-29 13:30:21 -0600

    add findByName and findByLatLong to MapService

--- static/js/data-download/services/MapService.js
+++ static/js/data-download/services/MapService.js
@@ -1,4 +1,4 @@
-var MapService = ['$collection', '$http', 'MAP_IMAGE_URL_PRE', function ($collection, $http, mapImageUrlPre)  {
+var MapService = ['$collection', '$http', 'MAP_IMAGE_URL_PRE', 'CartoService', function ($collection, $http, mapImageUrlPre, CartoService)  {
   'use strict';
 
   MapService = {};
@@ -14,6 +14,26 @@ var MapService = ['$collection', '$http', 'MAP_IMAGE_URL_PRE', function ($collec
       src: src
     };
   };
+
+  MapService.findByName = function (type, name) {
+    var table = CartoService.tableName(type);
+    var field = CartoService.nameField(type);
+
+    var query = 'SELECT ' + field + ', ST_AsGeoJSON(the_geom) from ' + table + ' WHERE ' + field + " = '" + name + "'";
+
+    return CartoService.sql(query);
+  };
+
+  MapService.findByLatLong = function (type, latitude, longitude) {
+    var table = CartoService.tableName(type);
+    var field = CartoService.nameField(type);
+
+    var query = 'SELECT ' + field + ', ST_AsGeojson(the_geom) as geojson from ' + table +
+      ' WHERE ST_Intersects(the_geom, ST_SetSRID(POINT(' + longitude + ', ' + latitude + ')::geometry, 4326))';
+
+    return CartoService.sql(query);
+  };
+
 
   return MapService;
 }];

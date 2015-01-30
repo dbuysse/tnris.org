bf378f77a2ebe8ff0f3799d964e13e6dfe8dd672
wilsaj <wilson.andrew.j+github@gmail.com>
2015-01-29 18:06:06 -0600

    return just matching quad row/geometry and normalize name field

--- static/js/data-download/services/MapService.js
+++ static/js/data-download/services/MapService.js
@@ -31,7 +31,19 @@ var MapService = ['$collection', '$http', 'MAP_IMAGE_URL_PRE', 'CartoService', f
     var query = 'SELECT ' + nameField + ', ST_AsGeojson(the_geom) as geojson from ' + table +
       ' WHERE ST_Intersects(the_geom, ST_SetSRID(POINT(' + longitude + ', ' + latitude + ')::geometry, 4326))';
 
-    return CartoService.sql(query);
+    return CartoService.sql(query)
+      .then(function (data) {
+        if (data.rows.length === 0) {
+          return {};
+        }
+        if (data.rows.length > 1) {
+          console.log("WARNING: More than 1 result returned for : " + type + '(' + latitude + ', ' + longitude + ')');
+          return {};
+        }
+        var obj = data.rows[0];
+        obj.name = obj[nameField];
+        return obj;
+      });
   };
 
 

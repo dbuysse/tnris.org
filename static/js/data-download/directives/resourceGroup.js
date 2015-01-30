9e39d652b16c9d508d46cd6fbfeef1c15b6d3876
James Seppi <james.seppi@gmail.com>
2015-01-30 15:13:41 -0600

    GA EventAction labels for downloads, fixes #177

--- static/js/data-download/directives/resourceGroup.js
+++ static/js/data-download/directives/resourceGroup.js
@@ -1,12 +1,29 @@
-var resourceGroup = ['DOWNLOAD_URL_PRE', function (downloadUrlPre) {
+var resourceGroup = ['$stateParams', 'DOWNLOAD_URL_PRE', function ($stateParams, downloadUrlPre) {
   return {
     restrict: 'EA',
     scope: {
-      group: '='
+      group: '=',
+      areaDataset: '='
     },
     templateUrl: 'partials/resourceGroup.html',
     link: function($scope) {
       $scope.downloadUrlPre = downloadUrlPre;
+
+      //EventAction strings for GA event tracking
+      switch ($scope.areaDataset.area) {
+        case 'statewide':
+          $scope.eventAction = 'STATE_Texas';
+          break;
+        case 'county':
+          $scope.eventAction = 'COUNTY_' + $stateParams.name;
+          break;
+        case 'quad':
+          $scope.eventAction = 'QUAD_' + $stateParams.name;
+          break;
+        case 'doqq':
+          $scope.eventAction = 'QQUAD_' + $stateParams.name;
+          break;
+      }
     }
   };
 }];
